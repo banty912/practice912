7d3b78cb1915d1badff9d43b75d9c798498a6bee
ANANDTY <guptaanand986@gmail.com>
2022-04-30 11:42:28 +0530

    user 1 second commit

---
+++ practice.github/src/test/java/contact/com/CreateLeadTest.java
@@ -0,0 +1,11 @@
+package contact.com;
+
+import org.testng.annotations.Test;
+
+public class CreateLeadTest {
+
+	@Test
+	public void createLeadOrg() {
+		System.out.println("create laed");
+	}
+}
---
+++ practice.github/src/test/java/practice/github/CreateContactTest.java
@@ -0,0 +1,10 @@
+package practice.github;
+
+import org.testng.annotations.Test;
+
+public class CreateContactTest {
+@Test
+public void createConTest() {
+	System.out.println("user1 second commit");
+}
+}
