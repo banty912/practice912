cad8538b0f6087035b4d55393ca2e7b6eb426324
ANANDTY <guptaanand986@gmail.com>
2022-04-30 11:10:16 +0530

    user1 first commit

---
+++ practice.github/src/test/java/practice/github/OrgTest.java
@@ -0,0 +1,14 @@
+package practice.github;
+
+import org.testng.annotations.Test;
+
+public class OrgTest {
+
+	@Test
+	public void createOrgm() {
+		System.out.println("user1 first commit is done");
+
+	}
+}
+
+
